64eb5a6e4b30ea8e57f8fe9bff390d2f7eb00a86
yash8mehra <yash8mehra@gmail.com>
2025-12-25 16:08:35 -0500

    all errors fixed and proper input validation

--- guessing_game.py
+++ guessing_game.py
@@ -1,7 +1,7 @@
 #Just trying to build a simple guessing game and testing
 
 #NEW THINGS TO ADD
-#Guess counter(done), quit option, replay without restarting, difficulty levels (number ranges)
+#Guess counter(done), quit option(Done), replay without restarting(play again button done), difficulty levels (number ranges)
 
 import random
 
@@ -9,47 +9,62 @@ print('WELCOME TO THE GUESSING GAME')
 print('ENTER ANY LETTER TO QUIT \n \n \n \n')
 
 
-def guess():
+def ValidInput():
+    tries = 0 #3 Tries to enter a valid number
+    while tries < 3:
+        UserNum = input("Enter any number from 0 to 100:          ")
+    
+        if not UserNum.isdigit():
+            print("Not a number enter numbers only: ")
+            tries += 1
+            continue
+    
+
+        if int(UserNum) < 0 or int(UserNum) > 100:
+            print("Number is out of bounds must be between 0 and 100\n")
+            tries += 1
+            continue
+        return int(UserNum)
+    
+    print("Too many attemps")
+    return None
+
+
+
+
+def Guess():
     RandomNum = random.randint(1,100)
-    print(RandomNum)
-    UserNum = int(input("Enter any number from 0 to 100:          "))
-    NumGuesses = 1
-    while UserNum != RandomNum:
+    print(f'random number is {RandomNum}') #For debugging purposes
+
+    NumGuesses = 0
+
+
+    while True:
+        UserNum = ValidInput()
+        if UserNum is None:
+            print("Game over, too many wrong attemps")
+            return
+        
         NumGuesses += 1
-        if (UserNum > 100) or (UserNum < 0):
-            print("Your number is out of bounds game over")
-            break
-        
-        elif not type(UserNum) is int:
-            raise TypeError("Awww sorry you had to quit \n")
-            break
 
+        if UserNum > RandomNum:
+            print("Too high")
+        elif UserNum < RandomNum:
+            print("Too low")
+        else:
+            print(f"YAY YOU WON THE NUMBER WAS:  {RandomNum}. It took you  {NumGuesses} guesses")
+            return
 
-
-        else:  
-        
-            if UserNum > RandomNum:
-                UserNum = int(input("Your number is too big, enter a smaller one:          "))
-
-            elif UserNum < RandomNum:
-                UserNum = int(input("Your number is too small, enter a larger number:           "))
-
-
-
-    if UserNum == RandomNum:
-            print("YAY YOU WON THE NUMBER WAS:  ", RandomNum, ". It took you ", NumGuesses, " guesses")
-    else:
-        print("YOU LOSE SORRY!! THE RIGHT NUMBER WAS:  ", RandomNum)
          
 
-guess()
+Guess()
 again = "Y"
 
 while again == "Y" or again == "y":
     again = input("\nDo you want to play again? Y/N:   ")
 
     if again == "Y" or again == 'y':
-        guess()
+        Guess()
     else:
         print("\nThankyou for playing with us have a great day!!")
 
